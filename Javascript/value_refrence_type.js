1e6ba91e2fc41210614812f054dc32c6bb904789
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-03-02 23:52:33 +0330

    remove extra closure 2 and add value refrence type file

---
+++ Javascript/value_refrence_type.js
@@ -0,0 +1,30 @@
+// --------> primitive type (value type) = doesn't change
+// let x = 10;
+// let x = 'one';
+// let x = null;
+// let x = false;
+
+// let y = x;
+
+// x = 20;
+// x = 'two';
+// x = undefined;
+// x = true;
+
+// --------> refrence type = gonna change
+// let x = {val: 10};
+// let x = [10]
+// function x() {
+//   return 10;
+// }
+
+// let y = x;
+
+// x.val = 20;
+// x[0] = 20;
+// function x() {
+//   return 20;
+// }
+
+// console.log(y())
+// console.log(y)
