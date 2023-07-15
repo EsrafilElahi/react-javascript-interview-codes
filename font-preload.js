288478e038f668dd05d423352c7d38dfe1d45b38
Esrafil <essinho1121@gmail.com>
2023-07-15 15:55:28 +0330

    Create font-preload.js

---
+++ font-preload.js
@@ -0,0 +1,17 @@
+// in nextjs --> _app.js
+<link
+  rel="preload"
+  href="/fonts/IranSans/Farsi_numerals/MobileFonts/IRANSansMobile(FaNum).ttf"
+  as="font"
+  crossorigin
+  type="font/ttf"
+/>
+
+// in nextjs --> _document.js
+<link
+  rel="preload"
+  href="/fonts/IranSans/Farsi_numerals/MobileFonts/IRANSansMobile(FaNum).ttf"
+  as="font"
+  crossorigin
+  type="font/ttf"
+/>
