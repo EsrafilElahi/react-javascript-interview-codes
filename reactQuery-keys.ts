472043ea5af16866fc2367d338104c563c45c53c
Esrafil <essinho1121@gmail.com>
2023-07-21 11:32:16 +0330

    Create reactQuery-keys.ts

---
+++ reactQuery-keys.ts
@@ -0,0 +1,7 @@
+const userKeys = {
+  all: ['users'] as const,
+  lists: () => [...userKeys.all, 'list'] as const,
+  list: (filters: string) => [...userKeys.lists(), { filters }] as const,
+  details: () => [...userKeys.all, 'detail'] as const,
+  detail: (id: number) => [...userKeys.details(), id] as const,
+};
