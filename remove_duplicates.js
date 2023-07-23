dcb31cf1e8f3b61081378a7d3066d63a5787445b
Esrafil <essinho1121@gmail.com>
2023-07-23 16:22:54 +0330

    Create remove_duplicates.js

---
+++ remove_duplicates.js
@@ -0,0 +1,9 @@
+const numbers = [3, 4, 2, 3, 7, 3];
+
+const filterArr2 = numbers.filter((item, index, arr) => arr.indexOf(item) === index) // way 1
+
+const filterArr3 = [...new Set(numbers)] // way 2
+
+const filterArr4 = numbers.reduce((acc, val) => {
+  acc.includes(val) ? acc : [...acc, val]
+},[]) // way 3
