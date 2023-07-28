4476626cb051242ca82280874cd2bf9c28888f71
Esrafil <essinho1121@gmail.com>
2023-07-28 15:22:13 +0330

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,10 @@
+Unit testing : Checks that individual units of code, mainly functions, work as expected ---> like a component
+tools: jest, @testing-library/react
+
+Integration testing: Individual units or features of the app are combined and tested as a group ---> like some components works together
+tools: jest, @testing-library/react
+
+End-to-end tests simulate actual user actions and are designed to test how a real user would likely use the application. 
+In React, E2E testing helps to ensure that the code you wrote is functional and your app works as intended, allowing you 
+to catch bugs in your code before your app is live. ---> an action like user's payment scenario
+tools: cypress
