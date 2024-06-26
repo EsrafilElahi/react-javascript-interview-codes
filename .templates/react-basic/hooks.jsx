388b07a473ae1793619c18b8b8566b28821edd1a
esrafil elahi <esrafil.elahi@gmail.com>
2024-06-26 11:39:48 +0330

    add template for react template

---
+++ .templates/react-basic/hooks.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+
+function use__templateNameToPascalCase__() {
+
+  return {}
+}
+
+export default use__templateNameToPascalCase__;
---
+++ .templates/react-constant/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function __templateNameToPascalCase__() {
+  return (
+    <div>
+      __templateNameToPascalCase__
+    </div>
+  )
+}
+
+export default __templateNameToPascalCase__;
